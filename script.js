bffc39cb8ecdb97c48277fcd70a7062f420f3458
Nguyen Dung <>
2022-01-19 07:34:04 +0700

    Feat: scrollbar

--- script.js
+++ script.js
@@ -12,10 +12,63 @@ function reveal() {
   }
 }
 
+function debounce(method, delay, arg) {
+  clearTimeout(method._tId);
+  method._tId= setTimeout(function(){
+      method(arg);
+  }, delay);
+}
+
+function scrollBar(i) {
+  const rotareArr = [0, 40, 80, 130];
+  const svgNavbar = document.querySelector('.navbar svg');
+  svgNavbar.style.transform = `rotate(${rotareArr[i]}deg)`;
+}
+
+function scrollSection() {
+  const sections = document.querySelectorAll('section');
+  for (let i = 0; i < sections.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = sections[i].getBoundingClientRect().top;
+    const elementVisible = 180;
+    if (elementTop < windowHeight - elementVisible) {
+      debounce(scrollBar, 50, i);
+      enableMenuBar(i);
+    }
+  }
+}
+
+function enableMenuBar(i) {
+  const menuDOM = document.querySelector('.menu');
+  Array.from(menuDOM.children).forEach((menuItem, ind) => {
+    if (i === ind) {
+      menuItem.classList.add('active');
+    } else {
+      menuItem.classList.remove('active');
+    }
+  });
+}
+
+function clickMenuBarScrollSection() {
+  const menuBar = document.querySelector('.menu');
+  const sections = document.querySelectorAll('section');
+  if (menuBar) {
+    Array.from(menuBar.children).forEach((menuItem, ind) => {
+      menuItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        sections[ind].scrollIntoView({ behavior: 'smooth' });
+      });
+    });
+  }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   const scrollContainerDOM = document.querySelector('.scroll-container');
   if (!scrollContainerDOM) return;
-  scrollContainerDOM.addEventListener("scroll", reveal);
+  scrollContainerDOM.addEventListener("scroll", function() {
+    reveal();
+    scrollSection();
+  });
   const galleryTab = [
     document.querySelector('.custom-bar .tab[data-content="#one"]'),
     document.querySelector('.custom-bar .tab[data-content="#two"]'),
@@ -54,4 +107,6 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     })
   });
+
+  clickMenuBarScrollSection();
 });
